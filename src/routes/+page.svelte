243fa2b1e37167b30472afaffc1cc459b87cfd27
Lane Sun <lanesun@anlbrain.com>
2025-01-14 14:33:31 +0800

    01-14

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,6 +69,7 @@
             const n_index = start + text.length;
             set_sel_pos(n_index);
             input.focus();
+            request_update();
         }, 0);
     }
 
@@ -160,6 +161,7 @@
                 {@const { linear, branch } = predictions}
                 <div class="flex px-2 py-1">
                     <div>{linear[0]}</div>
+                    , disable: true
                     <div class="op-50">{linear.slice(1)}</div>
                 </div>
                 <div class="grid rows-4 grid-flow-col p-1 gap-0.5 font-mono">
